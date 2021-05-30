7418e4cd7c3c8d115d7a4c1a0715048cc4de355f
HJfod <60038575+HJfod@users.noreply.github.com>
2021-05-30 14:23:57 +0300

    moved extra funcs to namespace

--- core/DefaultStrings.cs
+++ core/DefaultStrings.cs
@@ -376,28 +376,28 @@ HeaderCredit +
 #include <Windows.h>
 
 namespace gdmake {
-    #ifdef GDMAKE_WACKY_FUNCS
-    template<typename T>
-    static T getChild(cocos2d::CCNode* x, int i) {
-        return static_cast<T>(x->getChildren()->objectAtIndex(i));
-    }
+    namespace extra {
+        template<typename T>
+        static T getChild(cocos2d::CCNode* x, int i) {
+            return static_cast<T>(x->getChildren()->objectAtIndex(i));
+        }
 
-    template <typename T, typename R>
-    T as(R const v) { return reinterpret_cast<T>(v); }
+        template <typename T, typename R>
+        T as(R const v) { return reinterpret_cast<T>(v); }
 
-    inline std::string operator"""" _s (const char* _txt, size_t) {
-        return std::string(_txt);
-    }
+        inline std::string operator"""" _s (const char* _txt, size_t) {
+            return std::string(_txt);
+        }
 
-    template<typename T, typename U> constexpr size_t offsetOf(U T::*member) {
-        return (char*)&((T*)nullptr->*member) - (char*)nullptr;
-    }
+        template<typename T, typename U> constexpr size_t offsetOf(U T::*member) {
+            return (char*)&((T*)nullptr->*member) - (char*)nullptr;
+        }
 
-    template<typename T>
-    constexpr const T& clamp( const T& v, const T& lo, const T& hi) {
-        return v < lo ? lo : hi < v ? hi : v;
+        template<typename T>
+        constexpr const T& clamp( const T& v, const T& lo, const T& hi) {
+            return v < lo ? lo : hi < v ? hi : v;
+        }
     }
-    #endif
 
     [[nodiscard]]
     static std::string readFileString(const std::string & _path) {
